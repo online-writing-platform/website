9b2d1bb6fd7d16d64ac930ce96520ea40c3aca0e
Fatemeh Damirchi <fa.damirchy@gmail.com>
2026-07-09 21:42:49 +0330

    moved health endpoint to routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,15 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 
+const healthRoutes = require("./routes/health.routes");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/health", (req, res) => {
-    res.json({
-        status: "OK",
-    });
-});
+app.use("/health", healthRoutes);
 
 module.exports = app;
