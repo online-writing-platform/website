c130089a99bc6bf2abc5ae41d4ba69c20b5fef67
Fatemeh Damirchi <fa.damirchy@gmail.com>
2026-07-10 11:40:21 +0330

    cleaned up App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,8 @@
-import { useEffect, useState } from "react";
-
 function App() {
-    const [backendStatus, setBackendStatus] = useState("Loading...");
-
-    useEffect(() => {
-        fetch("http://localhost:5000/health")
-            .then((response) => {
-                return response.json();
-            })
-            .then((data) => {
-                setBackendStatus(data.status);
-            })
-            .catch((error) => {
-                setBackendStatus("ERROR");
-                console.log(error);
-            });
-    }, []);
-
     return (
         <main>
-            <h1>Backend Status</h1>
-            <p>{backendStatus}</p>
+            <h1>Online Writing Platform</h1>
+            <p>Initial project setup is ready.</p>
         </main>
     );
 }
