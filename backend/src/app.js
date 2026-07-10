e90c58492dac213b69465cca60483007be8eff24
Fatemeh Damirchi <fa.damirchy@gmail.com>
2026-07-10 10:21:42 +0330

    added new routes to app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,6 +4,9 @@ const cors = require("cors");
 const healthRoutes = require("./routes/health.routes");
 const authRoutes = require("./routes/auth.routes");
 
+const notFoundHandler = require("./middlewares/notFound.middleware");
+const errorHandler = require("./middlewares/error.middleware");
+
 const app = express();
 
 app.use(cors());
@@ -11,5 +14,8 @@ app.use(express.json());
 
 app.use("/health", healthRoutes);
 app.use("/auth", authRoutes);
+
+app.use(notFoundHandler);
+app.use(errorHandler);
 
 module.exports = app;
