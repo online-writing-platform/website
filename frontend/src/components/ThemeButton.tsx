8dc717303bfe4603ed0233b776a9f799e9958063
Mahdiye <mahdieh.hadi.8484@gmail.com>
2026-08-02 01:39:02 +0330

    add style to button

--- frontend/src/components/ThemeButton.tsx
+++ frontend/src/components/ThemeButton.tsx
@@ -1,13 +1,23 @@
 import { useTheme } from "../context/theme";
-
+import { FiMoon, FiSun } from "react-icons/fi";
+import "./ThemeButton.css";
 function ThemeButton() {
-    const { theme, toggleTheme } = useTheme();
+  const { theme, toggleTheme } = useTheme();
 
-    return (
-        <button type="button" onClick={toggleTheme} aria-label="تغییر پوسته">
-            {theme === "light" ? "🌙" : "☀️"}
-        </button>
-    );
+  return (
+    <button
+      className="theme-button"
+      type="button"
+      onClick={toggleTheme}
+      aria-label="تغییر پوسته"
+    >
+      {theme === "light" ? (
+        <FiMoon className="moon" />
+      ) : (
+        <FiSun className="sun" />
+      )}
+    </button>
+  );
 }
 
 export default ThemeButton;
