5a7228d4be0d230c6b74ceedef8accc0e564d5bc
Mahdiye <mahdieh.hadi.8484@gmail.com>
2026-08-03 18:31:39 +0330

    rewriting App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,10 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import "./App.css";
+
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 import About from "./pages/About";
 import Contact from "./pages/Contact";
@@ -22,15 +25,46 @@ function App() {
 
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/profile" element={<Profile />} />
+
         <Route path="/about" element={<About />} />
+
         <Route path="/terms" element={<Terms />} />
+
         <Route path="/register" element={<Register />} />
+
         <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/writestory" element={<WriteStory />} />
+
         <Route path="/search" element={<Search />} />
+
         <Route path="/contact" element={<Contact />} />
+
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/writestory"
+          element={
+            <ProtectedRoute>
+              <WriteStory />
+            </ProtectedRoute>
+          }
+        />
+
         <Route path="*" element={<ErrorPage />} />
       </Routes>
 
